6526bbd0915ae9064afc91e08efe636a228b36bd
Viktor <vikt.mag@gmail.com>
2023-02-06 19:33:36 +0100

    Added function to import benchmarks from json

---
+++ main.py
@@ -0,0 +1,8 @@
+import json
+
+def import_benchmark_json(benchmark_type):
+    with open(f"benchmarks/latest/{benchmark_type}.json", "r") as f:
+        data = json.load(f)
+    return data
+
+print(import_benchmark_json("GPU"))
