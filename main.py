599e63e921374f0cf379915b7868b1fa5cbf4362
Viktor <vikt.mag@gmail.com>
2023-02-11 22:08:48 +0100

    Added support for multiple html pages when fetching price

--- main.py
+++ main.py
@@ -11,10 +11,12 @@ def import_benchmark_json(benchmark_type):
     return data
 
 def get_prices_test():
-    # 2 Pages (3080)
-    url = "https://www.prisjakt.nu/c/grafikkort?532=36254"
+    # 1 Page (3080)
+    # url = "https://www.prisjakt.nu/c/grafikkort?532=36254"
     # 1 Page (4090)
     # url = "https://www.prisjakt.nu/c/grafikkort?532=39780"
+    # 17 Pages (NVIDIA GeForce Grafikkort)
+    url = "https://www.prisjakt.nu/c/grafikkort?103551=36436"
 
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
@@ -26,55 +28,59 @@ def get_prices_test():
     else:
         print("No link")
 
+    # TODO: Change to return SOUP OBJECT instead of writing file
+    with open("pjmultpagetest3.html", "w", encoding="utf-8") as file:
+        file.write(soup.prettify())
 
-    # with open("pj.html", "w", encoding="utf-8") as file:
-    #     file.write(soup.prettify())
-
-def local_page_test():
-    with open("pj.html", "r", encoding="utf-8") as file:
-        soup = BeautifulSoup(file, "html.parser")
+def write_local_json_file(page_list):
+    for index, page in enumerate(page_list, start=1):
+        # TODO: Change to SOUP OBJECT instead of file
+        with open(page, "r", encoding="utf-8") as file:
+            soup = BeautifulSoup(file, "html.parser")
     
-    page_json = soup.find_all("script")[5].text
+        page_json = soup.find_all("script")[5].text
 
-    start_text = r'{"__typename":"ProductsSlice"'
-    end_text = r',{"__typename":"DescriptionSlice"'
-    price_data = re.search(f"{start_text}.*?(?={end_text})", page_json).group(0)
-    # price_data = re.search(f"{start_text}(.*?){end_text}", page_json).group(1)
+        start_text = r'{"__typename":"ProductsSlice"'
+        end_text = r',{"__typename":"DescriptionSlice"'
+        price_data = re.search(f"{start_text}.*?(?={end_text})", page_json).group(0)
+        # price_data = re.search(f"{start_text}(.*?){end_text}", page_json).group(1)
 
-    price_data = price_data.replace("\\", "")
-    json_data = json.loads(price_data)
+        price_data = price_data.replace("\\", "")
+        json_data = json.loads(price_data)
 
-    with open("pjtest.json", "w") as file:
-        json.dump(json_data, file, indent=4)
+        with open(f"pjmultpagejson{index}.json", "w") as file:
+            json.dump(json_data, file, indent=4)
 
-    # tag = soup.find("a", {"aria-label": "Visa nästa"})
-    # if tag:
-    #     href = f"https://www.prisjakt.nu{tag['href']}"
-    #     print(href)
-    # else:
-    #     print("none")
 
-def get_gpu_price_list():
-    with open("pjtest.json") as file:
-        products_json = json.load(file)
-
+def get_gpu_price_list(page_list):
     price_list = []
 
-    for product in products_json["products"]:
-        if product["priceSummary"]["regular"] != None:
-            product_id = product["id"]
-            product_link = f"https://www.prisjakt.nu/produkt.php?p={product_id}"
-            product_price = int(product["priceSummary"]["regular"])
-            price_list.append((product_link, product_price))
+    for page in page_list:
+        # TODO: Change to JSON OBJECT instead of file
+        with open(page) as file:
+            products_json = json.load(file)
+
+        for product in products_json["products"]:
+            if product["priceSummary"]["regular"] != None:
+                product_id = product["id"]
+                product_link = f"https://www.prisjakt.nu/produkt.php?p={product_id}"
+                product_price = int(product["priceSummary"]["regular"])
+                price_list.append((product_link, product_price))
 
     sorted_price_list = sorted(price_list, key = lambda x: x[1])
     slice_num = 4
     list_slicer = int(len(sorted_price_list) / slice_num)
 
     sorted_price_list = sorted_price_list[:list_slicer]
-    print(sorted_price_list)
+    
+    return sorted_price_list
 
-get_gpu_price_list()
+pj_pages = ["pjmultpagetest.html", "pjmultpagetest2.html", "pjmultpagetest3.html"]
+pj_json = ["pjmultpagejson1.json", "pjmultpagejson2.json", "pjmultpagejson3.json"]
 
-# local_page_test()
+print(get_gpu_price_list(pj_json))
+
+# write_local_json_file(pj_pages)
+
+
 # get_prices_test()
