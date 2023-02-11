c87cd5f2831e5e30e37d408235bfd38785248134
Viktor <vikt.mag@gmail.com>
2023-02-12 00:21:53 +0100

    Changed get_gpu_price_list function to either read a list of local json files or a list of json objects

--- main.py
+++ main.py
@@ -86,14 +86,20 @@ def write_local_json_files(json_list):
         with open(f"pj_json_{current_time}_{i}.json", "w") as file:
             json.dump(json_file, file, indent=4)
 
-def get_gpu_price_list(page_list):
+def get_gpu_price_list(json_list, *, read_local_json_list=False):
+    # FROM LOCAL JSON FILES 
+    # json_list is list of strings of file paths
+    if read_local_json_list:
+        local_json_list = json_list
+        json_list = []
+        for json_file in local_json_list:
+            with open(json_file) as file:
+                products_json = json.load(file)
+                json_list.append(products_json)
+
     price_list = []
 
-    for page in page_list:
-        # TODO: Change to JSON OBJECT instead of file
-        with open(page) as file:
-            products_json = json.load(file)
-
+    for products_json in json_list:
         for product in products_json["products"]:
             if product["priceSummary"]["regular"] != None:
                 product_id = product["id"]
@@ -122,7 +128,7 @@ pj_json = ["pjmultpagejson1.json", "pjmultpagejson2.json", "pjmultpagejson3.json
 # 1 Page (3080)
 # url = "https://www.prisjakt.nu/c/grafikkort?532=36254"
 # 1 Page (4090)
-# url = "https://www.prisjakt.nu/c/grafikkort?532=39780"
+url = "https://www.prisjakt.nu/c/grafikkort?532=39780"
 # 17 Pages (NVIDIA GeForce Grafikkort)
 # url = "https://www.prisjakt.nu/c/grafikkort?103551=36436"
 # 4 Pages
@@ -130,9 +136,13 @@ pj_json = ["pjmultpagejson1.json", "pjmultpagejson2.json", "pjmultpagejson3.json
 # 2 Pages
 # url = "https://www.prisjakt.nu/c/optiska-enheter-for-datorer?557=1093"
 
-# html_soup_list = fetch_html_page(url)
-# json_list = create_json_list(html_soup_list)
+html_soup_list = fetch_html_page(url)
+json_list = create_json_list(html_soup_list)
+print(get_gpu_price_list(json_list))
 
-json_list = create_json_list(pj_pages_2, read_local_files=True)
+# write_local_json_files(json_list)
 
-write_local_json_files(json_list)
+# json_list = create_json_list(pj_pages_2, read_local_files=True)
+
+# print(get_gpu_price_list(pj_json, read_local_json_list=True))
+
