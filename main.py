cb1ebc1c77fbe833a07092e6330cc676d8043e0b
Viktor <vikt.mag@gmail.com>
2023-02-14 23:18:18 +0100

    Created gpu_pj_url_dict

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import re
 import datetime
 import time
 
-
 def import_benchmark_json(benchmark_type):
     with open(f"benchmarks/latest/{benchmark_type}.json", "r") as file:
         data = json.load(file)
@@ -173,15 +172,47 @@ def get_product_list_prices(product_link_list):
     return store_price_list
 
 
-pj_pages = ["pjmultpagetest.html", "pjmultpagetest2.html", "pjmultpagetest3.html"]
-pj_pages_2 = ["pjtest_2023-02-11_23-22-24.html", "pjtest_2023-02-11_23-22-25.html"]
-pj_json = ["pjmultpagejson1.json", "pjmultpagejson2.json", "pjmultpagejson3.json"]
+gpu_benchmarks = import_benchmark_json("GPU")
+gpu_pj_url_dict = {
+    "GeForce RTX 4090": "https://www.prisjakt.nu/c/grafikkort?532=39780",
+    "GeForce RTX 4080": "https://www.prisjakt.nu/c/grafikkort?532=39779",
+    "Radeon RX 7900 XTX": "https://www.prisjakt.nu/c/grafikkort?532=39908",
+    "GeForce RTX 4070 Ti": "https://www.prisjakt.nu/c/grafikkort?532=40333",
+    "Radeon RX 6950 XT": "https://www.prisjakt.nu/c/grafikkort?532=39794",
+    "Radeon RX 7900 XT": "https://www.prisjakt.nu/c/grafikkort?532=39907",
+    "Radeon RX 6800 XT": "https://www.prisjakt.nu/c/grafikkort?532=36621",
+    "GeForce RTX 3070 Ti": "https://www.prisjakt.nu/c/grafikkort?532=36434",
+    "GeForce RTX 3070": "https://www.prisjakt.nu/c/grafikkort?532=36253",
+    "Radeon RX 6800": "https://www.prisjakt.nu/c/grafikkort?532=36622",
+    "Radeon RX 6750 XT": "https://www.prisjakt.nu/c/grafikkort?532=39911",
+    "GeForce RTX 3060 Ti": "https://www.prisjakt.nu/c/grafikkort?532=36433",
+    "Radeon RX 6700 XT": "https://www.prisjakt.nu/c/grafikkort?532=36619",
+    "Radeon RX 6700": "https://www.prisjakt.nu/c/grafikkort?532=36620",
+    "Radeon RX 6650 XT": "https://www.prisjakt.nu/c/grafikkort?532=39910",
+    "GeForce RTX 3060": "https://www.prisjakt.nu/c/grafikkort?532=36256",
+    "Radeon RX 6600 XT": "https://www.prisjakt.nu/c/grafikkort?532=37636",
+    "Radeon RX 6600": "https://www.prisjakt.nu/c/grafikkort?532=37787",
+    "GeForce RTX 2060": "https://www.prisjakt.nu/c/grafikkort?532=32050",
+    "GeForce RTX 3050": "https://www.prisjakt.nu/c/grafikkort?532=38072",
+    "GeForce GTX 1660 Ti": "https://www.prisjakt.nu/c/grafikkort?532=32120",
+    "GeForce GTX 1660": "https://www.prisjakt.nu/c/grafikkort?532=32119",
+    "Radeon RX 6500 XT": "https://www.prisjakt.nu/c/grafikkort?532=38073",
+    "Radeon RX 6400": "https://www.prisjakt.nu/c/grafikkort?532=39913",
+}
+
+
+# pj_pages = ["pjmultpagetest.html", "pjmultpagetest2.html", "pjmultpagetest3.html"]
+# pj_pages_2 = ["pjtest_2023-02-11_23-22-24.html", "pjtest_2023-02-11_23-22-25.html"]
+# pj_json = ["pjmultpagejson1.json", "pjmultpagejson2.json", "pjmultpagejson3.json"]
 
 # print(get_gpu_price_list(pj_json))
 
 # write_local_json_file(pj_pages)
 
 # test_local_html_page()
+
+
+
 
 # 1 Page (3080)
 # url = "https://www.prisjakt.nu/c/grafikkort?532=36254"
@@ -196,8 +227,10 @@ pj_json = ["pjmultpagejson1.json", "pjmultpagejson2.json", "pjmultpagejson3.json
 # Product Page (Gigabyte GeForce RTX 4090 Gaming OC HDMI 3x DP 24GB)
 # url = "https://www.prisjakt.nu/produkt.php?p=7124177"
 
-gpu_price_list = get_gpu_category_price_list(["pj_json_2023-02-12_00-30-01_1.json"], read_local_json_list=True)[:2]
-products_in_stock_list = get_product_list_prices(gpu_price_list)
+# gpu_price_list = get_gpu_category_price_list(["pj_json_2023-02-12_00-30-01_1.json"], read_local_json_list=True)[:2]
+# products_in_stock_list = get_product_list_prices(gpu_price_list)
+# for product in products_in_stock_list:
+#     print(product)
 # product_list = ["https://www.prisjakt.nu/produkt.php?p=7124177", "https://www.prisjakt.nu/produkt.php?p=7123378", "https://www.prisjakt.nu/produkt.php?p=7123212"]
 
 
