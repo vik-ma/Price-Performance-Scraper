63d3ff7d3c02ed8745659bef9079e6b9aff61900
Viktor <vikt.mag@gmail.com>
2023-02-15 09:12:44 +0100

    Added performance tiers for CPUs

--- main.py
+++ main.py
@@ -94,6 +94,121 @@ cpu_pj_url_dict = {
     "Intel Core i5-12400": "https://www.prisjakt.nu/produkt.php?p=5948016",
 }    
 
+cpu_normal_tier_dict = {
+    "TOP TIER":{
+        "AMD Ryzen 9 5950X",
+        "AMD Ryzen 9 7950X",
+        "AMD Ryzen 9 7900X",
+        "AMD Ryzen 9 7900",
+        "Intel Core i9-13900KS",
+        "Intel Core i9-13900K",
+        "Intel Core i9-13900KF",
+        "Intel Core i9-13900F",
+        "Intel Core i9-13900",
+        "Intel Core i7-13700K",
+        "Intel Core i7-13700KF",
+    },
+    "HIGH TIER": {
+        "AMD Ryzen 9 5900X",
+        "AMD Ryzen 7 7700X",
+        "AMD Ryzen 7 7700",
+        "Intel Core i9-12900KS",
+        "Intel Core i9-12900K",
+        "Intel Core i9-12900KF",
+        "Intel Core i7-13700",
+        "Intel Core i7-13700F",
+        "Intel Core i5-13600K",
+        "Intel Core i5-13600KF",
+        "Intel Core i9-12900F",
+        "Intel Core i9-12900",
+        "Intel Core i7-12700K",
+        "Intel Core i7-12700KF",
+    },
+    "MID TIER": {
+        "AMD Ryzen 7 5800X",
+        "AMD Ryzen 7 5800X3D",
+        "AMD Ryzen 7 5700X",
+        "AMD Ryzen 7 5700G",
+        "AMD Ryzen 5 7600X",
+        "AMD Ryzen 5 7600",
+        "Intel Core i5-13500",
+        "Intel Core i7-12700F",
+        "Intel Core i7-12700",
+        "Intel Core i5-12600K",
+        "Intel Core i5-12600KF",
+        "Intel Core i5-13400",
+        "Intel Core i5-13400F",
+    },
+    "LOW TIER": {
+        "AMD Ryzen 5 5600X",
+        "AMD Ryzen 5 5600",
+        "Intel Core i5-12600",
+        "Intel Core i5-12500",
+        "Intel Core i5-12400F",
+        "Intel Core i5-12400",
+        "AMD Ryzen 5 5600G",
+        "AMD Ryzen 5 5500",   
+    },
+}
+
+cpu_gaming_tier_dict = {
+    "TOP TIER":{
+        "Intel Core i9-13900KS",
+        "Intel Core i9-13900K",
+        "Intel Core i9-13900KF",
+    },
+    "HIGH TIER": {
+        "AMD Ryzen 7 5800X3D",
+        "AMD Ryzen 9 7900X",
+        "AMD Ryzen 5 7600X",
+        "AMD Ryzen 5 7600",
+        "AMD Ryzen 9 7950X",
+        "AMD Ryzen 7 7700X",
+        "Intel Core i7-13700K",
+        "Intel Core i7-13700KF",
+        "Intel Core i7-13700",
+        "Intel Core i7-13700F",
+        "Intel Core i5-13600K",
+        "Intel Core i5-13600KF",
+        "Intel Core i9-12900KS",
+        "Intel Core i9-12900K",
+        "Intel Core i9-12900KF",
+    },
+    "MID TIER": {
+        "AMD Ryzen 9 5900X",
+        "AMD Ryzen 5 5600X",
+        "Intel Core i9-12900F",
+        "Intel Core i9-12900",
+        "Intel Core i5-12600K",
+        "Intel Core i7-12700K",
+        "Intel Core i5-13500",
+        "Intel Core i5-12600KF",
+        "Intel Core i7-12700KF",
+        "Intel Core i5-13400",
+    },
+    "LOW TIER": {
+        "AMD Ryzen 5 5600",
+        "AMD Ryzen 7 5800X",
+        "AMD Ryzen 9 5950X",
+        "AMD Ryzen 7 5700X",
+        "Intel Core i5-12600",
+        "Intel Core i7-12700",
+        "Intel Core i5-13400F",
+        "Intel Core i7-12700F",
+        "Intel Core i5-12500",
+        "Intel Core i5-12400F",
+        "Intel Core i5-12400",
+    },
+    "BOTTOM TIER": {
+        "AMD Ryzen 7 5700G",
+        "AMD Ryzen 5 5600G",
+        "AMD Ryzen 5 5500",
+        "AMD Ryzen 9 7900",
+        "AMD Ryzen 7 7700"
+    }
+}
+
+
 def import_benchmark_json(benchmark_type):
     with open(f"benchmarks/latest/{benchmark_type}.json", "r") as file:
         data = json.load(file)
@@ -137,19 +252,27 @@ def fetch_product_page(url):
     soup = BeautifulSoup(response.text, "html.parser")
 
     return soup
-    # save_local_html_page(soup, "pjproductpage.html")
+    # save_local_html_page(soup, "pjproductpagecpu.html")
 
 
 def test_local_html_page():
-    with open("pjproductpage.html", "r", encoding="utf-8") as file:
+    with open("pjproductpagecpu.html", "r", encoding="utf-8") as file:
         soup = BeautifulSoup(file, "html.parser")
 
-    # write_local_json_files([json_data])
+    json_data = get_product_json(soup)
+
+    write_local_json_files([json_data])
 
 
 def test_local_json_file():
-    with open("pj_json_2023-02-13_17-50-45_1.json", "r") as file:
+    with open("pj_json_2023-02-15_08-38-43_1.json", "r") as file:
         json_data = json.load(file)
+    
+    product_price_list = get_product_price_list(json_data, "ASD")
+
+    for product in product_price_list:
+        print(product)
+
 
 
 def get_product_json(soup):
@@ -307,9 +430,10 @@ def start_price_fetching_gpu(tier_choice):
         print(entry)
 
 
-
-
-
+print(sum(len(v) for v in cpu_normal_tier_dict.values()))
+print(sum(len(v) for v in cpu_gaming_tier_dict.values()))
+# fetch_product_page(cpu_pj_url_dict["Intel Core i9-13900KS"])
+# test_local_json_file()
 
 # start_price_fetching_gpu(gpu_pj_url_dict["UPPER MID TIER"])
 
