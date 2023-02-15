7d606bd83b7e8fce682e473405dccb48e5cd68a8
Viktor <vikt.mag@gmail.com>
2023-02-15 09:18:43 +0100

    Added socket dict for CPUs

--- main.py
+++ main.py
@@ -94,6 +94,61 @@ cpu_pj_url_dict = {
     "Intel Core i5-12400": "https://www.prisjakt.nu/produkt.php?p=5948016",
 }    
 
+cpu_socket_dict = {
+    "Socket AM4": {
+        "AMD Ryzen 9 5950X",
+        "AMD Ryzen 9 5900X",
+        "AMD Ryzen 7 5800X",
+        "AMD Ryzen 7 5800X3D",
+        "AMD Ryzen 7 5700X",
+        "AMD Ryzen 7 5700G",
+        "AMD Ryzen 5 5600X",
+        "AMD Ryzen 5 5600",
+        "AMD Ryzen 5 5600G",
+        "AMD Ryzen 5 5500",
+    },
+    "Socket AM5": {
+        "AMD Ryzen 9 7950X",
+        "AMD Ryzen 9 7900X",
+        "AMD Ryzen 9 7900",
+        "AMD Ryzen 7 7700X",
+        "AMD Ryzen 7 7700",
+        "AMD Ryzen 5 7600X",
+        "AMD Ryzen 5 7600",
+    },
+    "Socket LGA 1700": {
+        "Intel Core i9-13900KS",
+        "Intel Core i9-13900K",
+        "Intel Core i9-13900KF",
+        "Intel Core i9-13900F",
+        "Intel Core i9-13900",
+        "Intel Core i7-13700K",
+        "Intel Core i7-13700KF",
+        "Intel Core i9-12900KS",
+        "Intel Core i9-12900K",
+        "Intel Core i9-12900KF",
+        "Intel Core i7-13700",
+        "Intel Core i7-13700F",
+        "Intel Core i5-13600K",
+        "Intel Core i5-13600KF",
+        "Intel Core i9-12900F",
+        "Intel Core i9-12900",
+        "Intel Core i7-12700K",
+        "Intel Core i7-12700KF",
+        "Intel Core i5-13500",
+        "Intel Core i7-12700F",
+        "Intel Core i7-12700",
+        "Intel Core i5-12600K",
+        "Intel Core i5-12600KF",
+        "Intel Core i5-13400",
+        "Intel Core i5-13400F",
+        "Intel Core i5-12600",
+        "Intel Core i5-12500",
+        "Intel Core i5-12400F",
+        "Intel Core i5-12400",
+    }
+}
+
 cpu_normal_tier_dict = {
     "TOP TIER":{
         "AMD Ryzen 9 5950X",
@@ -432,6 +487,7 @@ def start_price_fetching_gpu(tier_choice):
 
 print(sum(len(v) for v in cpu_normal_tier_dict.values()))
 print(sum(len(v) for v in cpu_gaming_tier_dict.values()))
+print(sum(len(v) for v in cpu_socket_dict.values()))
 # fetch_product_page(cpu_pj_url_dict["Intel Core i9-13900KS"])
 # test_local_json_file()
 
