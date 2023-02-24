de670386e4d02e1c32267dba0baa0c168795af8f
Viktor <vikt.mag@gmail.com>
2023-02-24 07:30:45 +0100

    Added test table to db

--- app/backend/price_fetcher/models.py
+++ app/backend/price_fetcher/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Test(models.Model):
+    test_name = models.CharField(max_length=30)
+    test_num = models.IntegerField()
+    test_date = models.DateField()
