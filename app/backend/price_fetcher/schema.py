001b6c708ea6d86624834688dbfbbb474bd33f31
Viktor <vikt.mag@gmail.com>
2023-03-01 03:26:38 +0100

    Added query to resolve ProductListings with specific timestamp_id

--- app/backend/price_fetcher/schema.py
+++ app/backend/price_fetcher/schema.py
@@ -8,7 +8,7 @@ class CompletedFetchType(DjangoObjectType):
         model = CompletedFetch
         fields = ("product_list", "benchmark_type", "timestamp", "timestamp_id")
 
-class ProductListing(DjangoObjectType):
+class ProductListingType(DjangoObjectType):
     class Meta:
         model = ProductListing
         fields = (
@@ -24,12 +24,12 @@ class ProductListing(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     all_completed_fetches = graphene.List(CompletedFetchType)
-    all_product_listings = graphene.List(ProductListing, id = graphene.String())
+    product_listings = graphene.List(ProductListingType, timestamp_id = graphene.String(required=True))
 
     def resolve_all_completed_fetches(self, info):
         return CompletedFetch.objects.all()
     
-    def resolve_all_product_listings(self, info, id):
-        return ProductListing.objects.filter(timestamp_id = id)
+    def resolve_product_listings(self, info, timestamp_id):
+        return ProductListing.objects.filter(timestamp_id = timestamp_id)
 
 schema = graphene.Schema(query=Query)
