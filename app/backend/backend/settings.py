0449169adab0c5aa9c32efb1921d5b9f8fad3f0a
Viktor <vikt.mag@gmail.com>
2023-03-03 07:28:55 +0100

    Installed django-cors-headers

--- app/backend/backend/settings.py
+++ app/backend/backend/settings.py
@@ -42,9 +42,11 @@ INSTALLED_APPS = [
     'graphene_django',
     'rest_framework',
     'api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -130,3 +132,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:5173'
+]
