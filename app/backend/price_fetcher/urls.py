e956945d4ee41f74ed5ee8101e5f50e80e2e6c17
Viktor <vikt.mag@gmail.com>
2023-03-05 22:50:40 +0100

    Added fetching from GraphQL to frontend

--- app/backend/price_fetcher/urls.py
+++ app/backend/price_fetcher/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from graphene_django.views import GraphQLView
 from . import views
 from price_fetcher.schema import schema
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('test_list/', views.list_test_items),
     path('insert_test/', views.insert_test_item, name='insert_test_item'),
     path('delete_test/<int:test_id>/', views.delete_test_item, name='delete_test_item'),
     path('test_button/', views.test_button, name='test_button'),
-    path('graphql/', GraphQLView.as_view(graphiql=True, schema=schema)),
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
 ]
--- app/frontend/app/page.tsx
+++ app/frontend/app/page.tsx
@@ -2,17 +2,43 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 // import styles from "./page.module.css";
 import Link from "next/link";
+import { gql } from "@apollo/client";
+import client from "../apollo-client";
 
 const inter = Inter({ subsets: ["latin"] });
 
 type TestApiProps = {
-  message: string
+  message: string;
+};
+
+type CompletedFetchProps = {
+  productList: string;
+  benchmarkType: string;
+  timestamp: string;
+  timestampId: string;
+};
+
+async function getCompletedFetches() {
+  const { data } = await client.query({
+    query: gql`
+      query {
+        allCompletedFetches {
+          productList
+          benchmarkType
+          timestamp
+          timestampId
+        }
+      }
+    `,
+  });
+
+  return data;
 }
 
 async function getApiData(): Promise<TestApiProps> {
-  const res = await fetch(`${process.env.DJANGO_URL}/api/test_frontend/`,
-  { cache: 'no-store' }
-  );
+  const res = await fetch(`${process.env.DJANGO_API_URL}/test_frontend/`, {
+    cache: "no-store",
+  });
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
@@ -22,14 +48,19 @@ async function getApiData(): Promise<TestApiProps> {
 }
 
 export default async function Home() {
-
   const apiData = await getApiData();
+  const gqlData = await getCompletedFetches();
 
   return (
     <main>
       <div>
         <h1>Home</h1>
         <p>API Test: {apiData.message}</p>
+        <ul>
+          {gqlData?.allCompletedFetches.map((fetch : CompletedFetchProps) => (
+            <li key={fetch.timestampId}>{fetch.productList}<br/>{fetch.benchmarkType} - {fetch.timestamp.substring(0, 10)}</li>
+          ))}
+        </ul>
         <div className="padding-top: 3em">
           <Link href="/fetches">View Completed Fetches</Link>
         </div>
