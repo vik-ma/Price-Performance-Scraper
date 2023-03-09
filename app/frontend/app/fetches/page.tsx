34685bfd44bd4e5fe193e1589a9a3f94d6cde2c6
Viktor <vikt.mag@gmail.com>
2023-03-09 06:01:27 +0100

    Added comprehensive list of completed fetches to /fetches

--- app/frontend/app/fetches/page.tsx
+++ app/frontend/app/fetches/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { gql } from "@apollo/client";
 import client from "../../apollo-client";
 import { CompletedFetchProps } from "@/typings";
+import Link from "next/link";
 
 async function getCompletedFetches() {
   const { data } = await client.query({
@@ -22,14 +23,27 @@ async function getCompletedFetches() {
 
 export default async function Fetches() {
   const gqlData = await getCompletedFetches();
-  
+
   return (
     <>
       <div className="fetchContent">
         <h1>Completed Fetches</h1>
-        {/* @ts-ignore */}
-        {/* <FetchesList /> */}
-        <p>asd</p>
+        <ul>
+          {gqlData
+            ?.slice(0)
+            .reverse()
+            .map((fetch: CompletedFetchProps) => (
+              <li className="fullFetchListItem" key={fetch.timestampId}>
+                <Link href={`/fetches/${fetch.timestampId}`}>
+                  <strong>{fetch.benchmarkType}</strong>
+                  <br />
+                  {fetch.productList}
+                  <br />
+                  <small>{fetch.timestamp.substring(0, 19).replace("T", " ")}</small>
+                </Link>
+              </li>
+            ))}
+        </ul>
       </div>
     </>
   );
