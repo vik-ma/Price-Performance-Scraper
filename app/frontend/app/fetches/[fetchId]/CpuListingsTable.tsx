4595b80a2e87fb0afb5c996f6fe179346e9b83ba
Viktor <vikt.mag@gmail.com>
2023-03-10 15:39:26 +0100

    Added sorting to CpuListingsTable

--- app/frontend/app/fetches/[fetchId]/CpuListingsTable.tsx
+++ app/frontend/app/fetches/[fetchId]/CpuListingsTable.tsx
@@ -1,21 +1,58 @@
 "use client";
 import React from "react";
+import { useState } from "react";
 import {
   CompletedFetchProps,
   ProductListingsProps,
   FetchPageProps,
 } from "@/typings";
 
+type ProductTableSortProps = {
+  SortKey: keyof ProductListingsProps;
+  SortDirection: "asc" | "desc";
+};
+
+type TableHeadingProps = {
+  Label: string;
+  Key: keyof ProductListingsProps;
+};
+
 export default function CpuListingsTable({
   params: { fetchInfo, productListings },
 }: FetchPageProps) {
-  const tableHeading = [
-    "Product",
-    "Store",
-    "Benchmark Value",
-    "Price",
-    "Price / Performance Score",
+  const tableHeading: TableHeadingProps[] = [
+    { Label: "Product", Key: "productName" },
+    { Label: "Store", Key: "storeName" },
+    { Label: "Benchmark Value", Key: "benchmarkValue" },
+    { Label: "Price", Key: "price" },
+    { Label: "Price / Performance Score", Key: "pricePerformanceRatio" },
   ];
+
+  const [sortTable, setSortTable] = useState<ProductTableSortProps>({
+    SortKey: "pricePerformanceRatio",
+    SortDirection: "desc",
+  });
+
+  const handleHeaderClick = (head: keyof ProductListingsProps) => {
+    setSortTable({
+      SortKey: head,
+      SortDirection:
+        head === sortTable.SortKey
+          ? sortTable.SortDirection === "asc"
+            ? "desc"
+            : "asc"
+          : "desc",
+    });
+  };
+
+  const sortedListings = [...productListings].sort((a, b) => {
+    const columnA = a[sortTable.SortKey];
+    const columnB = b[sortTable.SortKey];
+    const direction = sortTable.SortDirection === "asc" ? 1 : -1;
+    if (columnA < columnB) return -1 * direction;
+    if (columnA > columnB) return 1 * direction;
+    return 0;
+  });
 
   return (
     <>
@@ -23,12 +60,29 @@ export default function CpuListingsTable({
         <thead>
           <tr>
             {tableHeading.map((head, headID) => (
-              <th key={headID}>{head}</th>
+              <th
+                key={headID}
+                onClick={
+                  headID === 0 || headID === 1
+                    ? undefined
+                    : () =>
+                        handleHeaderClick(
+                          head.Key as keyof ProductListingsProps
+                        )
+                }
+              >
+                {head.Label}{" "}
+                {sortTable.SortKey === head.Key
+                  ? sortTable.SortDirection === "asc"
+                    ? "↑"
+                    : "↓"
+                  : ""}
+              </th>
             ))}
           </tr>
         </thead>
         <tbody>
-          {productListings?.map(
+          {sortedListings?.map(
             (listing: ProductListingsProps, index: number) => (
               <tr key={index}>
                 {listing.productLink !== "" ? (
