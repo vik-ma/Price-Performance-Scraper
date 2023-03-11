c037ba2342a0ad2e9f0bb6c38a11f785966c411a
Viktor <vikt.mag@gmail.com>
2023-03-11 23:24:54 +0100

    Added error screen for fetches pages

---
+++ app/frontend/app/fetches/error.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="fetchContent">
+      <h2>Something went wrong!</h2>
+      <button
+        onClick={
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+    </div>
+  );
+}
