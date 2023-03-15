cdd89178036dc1f597eb7f63d3d19768daf4c772
Viktor <vikt.mag@gmail.com>
2023-03-15 16:17:00 +0100

    Created ProductInfo.tsx and added gpuInfo

---
+++ app/frontend/app/ProductInfo.tsx
@@ -0,0 +1,131 @@
+export const gpuInfo = {
+    "GeForce RTX 4090":
+        {
+            "tier": "TIER 1",
+            "cssName": "rtx-4090"
+        },
+    "GeForce RTX 4080":
+        {
+            "tier": "TIER 1",
+            "cssName": "rtx-4080"
+        },
+    "Radeon RX 7900 XTX":
+        {
+            "tier": "TIER 1",
+            "cssName": "rx-7900-xtx"
+        },
+    "GeForce RTX 4070 Ti":
+        {
+            "tier": "TIER 2",
+            "cssName": "rtx-4070-ti"
+        },
+    "Radeon RX 6950 XT":
+        {
+            "tier": "TIER 2",
+            "cssName": "rx-6950-xt"
+        },
+    "Radeon RX 7900 XT":
+        {
+            "tier": "TIER 2",
+            "cssName": "rx-7900-xt"
+        },
+    "Radeon RX 6800 XT":
+        {
+            "tier": "TIER 2",
+            "cssName": "rx-6800-xt"
+        },
+    "GeForce RTX 3070 Ti":
+        {
+            "tier": "TIER 3",
+            "cssName": "rtx-3070-ti"
+        },
+    "GeForce RTX 3070":
+        {
+            "tier": "TIER 3",
+            "cssName": "rtx-3070"
+        },
+    "Radeon RX 6800":
+        {
+            "tier": "TIER 3",
+            "cssName": "rx-6800"
+        },
+    "Radeon RX 6750 XT":
+        {
+            "tier": "TIER 3",
+            "cssName": "rx-6750-xt"
+        },
+    "GeForce RTX 3060 Ti":
+        {
+            "tier": "TIER 4",
+            "cssName": "rtx-3060-ti"
+        },
+    "Radeon RX 6700 XT":
+        {
+            "tier": "TIER 4",
+            "cssName": "rx-6700-xt"
+        },
+    "Radeon RX 6700":
+        {
+            "tier": "TIER 4",
+            "cssName": "rx-6700"
+        },
+    "Radeon RX 6650 XT":
+        {
+            "tier": "TIER 4",
+            "cssName": "rx-6650-xt"
+        },
+    "GeForce RTX 3060":
+        {
+            "tier": "TIER 5",
+            "cssName": "rtx-3060"
+        },
+    "Radeon RX 6600 XT":
+        {
+            "tier": "TIER 5",
+            "cssName": "rx-6600-xt"
+        },
+    "Radeon RX 6600":
+        {
+            "tier": "TIER 5",
+            "cssName": "rx-6600"
+        },
+    "GeForce RTX 2060":
+        {
+            "tier": "TIER 6",
+            "cssName": "rtx-2060"
+        },
+    "GeForce GTX 1660 Super":
+        {
+            "tier": "TIER 6",
+            "cssName": "gtx-1660-super"
+        },
+    "GeForce RTX 3050":
+        {
+            "tier": "TIER 6",
+            "cssName": "rtx-3050"
+        },
+    "GeForce GTX 1660 Ti":
+        {
+            "tier": "TIER 6",
+            "cssName": "gtx-1660-ti"
+        },
+    "GeForce GTX 1660":
+        {
+            "tier": "TIER 7",
+            "cssName": "gtx-1660"
+        },
+    "Radeon RX 6500 XT":
+        {
+            "tier": "TIER 7",
+            "cssName": "rx-6500-xt"
+        },
+    "Radeon RX 6400":
+        {
+            "tier": "TIER 7",
+            "cssName": "rx-6400"
+        },
+}
+
+export const cpuInfo = {
+
+}
