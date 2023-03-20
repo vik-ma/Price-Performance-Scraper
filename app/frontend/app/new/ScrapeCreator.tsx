14ada77471bbc3f36df34a659617953e1a7a67cb
Viktor <vikt.mag@gmail.com>
2023-03-20 05:20:04 +0100

    Added set to add GPU products to

--- app/frontend/app/new/ScrapeCreator.tsx
+++ app/frontend/app/new/ScrapeCreator.tsx
@@ -13,22 +13,46 @@ export default function ScrapeCreator(scrapeType: ScrapeType) {
       : "GPU";
 
   const tiers = new Set(Object.values(gpuInfo).map((gpu) => gpu.tier));
+
+  const [selectedItems, setSelectedItems] = useState(new Set<string>([]));
+
+  const handleItemClick = (name: string) => {
+    setSelectedItems(
+      (prevSelectedItems) => new Set(prevSelectedItems.add(name))
+    );
+  };
+
+
+
   return (
     <>
       <h2>{scrapeTypeTitle}</h2>
+      <h2>Selected Items</h2>
       <div className="tiers-container">
         {Array.from(tiers).map((tier) => (
           <div className="tiers-item" key={tier}>
-            <h3 className={`tiers-title text-color-tier-${tier}`}>Tier {tier}</h3>
+            <h3 className={`tiers-title text-color-tier-${tier}`}>
+              Tier {tier}
+            </h3>
             <ul>
               {Object.entries(gpuInfo)
-                .filter(([name, gpu]) => gpu.tier === tier)
+                .filter(([name, gpu]) => gpu.tier === tier && !selectedItems.has(name))
                 .map(([name, gpu]) => (
-                  <li key={name}><strong>{name}</strong></li>
+                  <li key={name}>
+                    <button onClick={() => handleItemClick(name)}>
+                      {name}
+                    </button>
+                  </li>
                 ))}
             </ul>
           </div>
         ))}
+        <h2>Selected Items</h2>
+        <ul>
+          {[...selectedItems].map((item) => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
       </div>
     </>
   );
