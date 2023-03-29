2fcc0120c92ece2dd8b925850daa735bc657e4cb
Viktor <vikt.mag@gmail.com>
2023-03-29 19:12:23 +0200

    Added row mapping to table in manual comaprison tool

--- app/frontend/app/manual_comparison/page.tsx
+++ app/frontend/app/manual_comparison/page.tsx
@@ -1,6 +1,13 @@
 "use client";
+import { useState } from "react";
 
 export default function ManualComparison() {
+  const [numRows, setNumRows] = useState<number>(1);
+
+  const rows = Array.from({ length: numRows });
+
+  const [ppsArray, setPpsArray] = useState<number[]>([]);
+
   return (
     <>
       <h1>Manual Comparison Tool</h1>
@@ -21,26 +28,34 @@ export default function ManualComparison() {
                 <strong>Price / Performance Score</strong>
               </th>
             </tr>
-          </thead>{" "}
+          </thead>
           <tbody>
-            <tr>
-              <td>
-                <label>
-                  <input type="text" placeholder="Name (Optional)" />
-                </label>
-              </td>
-              <td>
-                <label>
-                  <input type="text" placeholder="Performance Value" required />
-                </label>
-              </td>
-              <td>
-                <label>
-                  <input type="text" placeholder="Price" required />
-                </label>
-              </td>
-              <td></td>
-            </tr>
+            {rows.map((_, index) => (
+              <tr key={index}>
+                <td>
+                  <label>
+                    <input type="text" placeholder="Name (Optional)" />
+                  </label>
+                </td>
+                <td>
+                  <label>
+                    <input
+                      type="text"
+                      placeholder="Performance Value"
+                      required
+                    />
+                  </label>
+                </td>
+                <td>
+                  <label>
+                    <input type="text" placeholder="Price" required />
+                  </label>
+                </td>
+                <td>
+                  <strong>{ppsArray[index]}</strong>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
         <button type="submit">Calculate</button>
