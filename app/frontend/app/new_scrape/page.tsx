324a1d947e1e8f2dc572d8fb2289dc26e1309838
Viktor <vikt.mag@gmail.com>
2023-04-02 20:13:54 +0200

    Removed unused code from new_scrape page.tsx

--- app/frontend/app/new_scrape/page.tsx
+++ app/frontend/app/new_scrape/page.tsx
@@ -1,30 +1,8 @@
 "use client";
 import React from "react";
 import { useState, useContext, createContext } from "react";
-import { useRouter } from "next/navigation";
 import ScrapeCreator from "./ScrapeCreator";
 
-async function testPostRequest(data = {}) {
-  const response = await fetch(`http://localhost:8000/api/test_post/`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
-  return response.json();
-}
-
-async function startPriceFetch(data = {}) {
-  const response = await fetch(`http://localhost:8000/api/start_price_fetch/`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
-  return response.json();
-}
 interface CreateScrapeContextType {
   loadingScrape: boolean;
   setLoadingScrape: React.Dispatch<React.SetStateAction<boolean>>;
@@ -44,62 +22,6 @@ export const CreateScrapeContext = createContext<CreateScrapeContextType>({
 });
 
 export default function NewScrape() {
-  const router = useRouter();
-
-  const handleClickTest = async () => {
-    // setLoading(true);
-    try {
-      const response = await testPostRequest({});
-
-      // setLoading(false);
-
-      if (response.hasOwnProperty("success")) {
-        if (response.success) {
-          router.push("/scrapes");
-        } else {
-          setPostReturn(
-            `'Success' exists and is false ${JSON.stringify(response)}`
-          );
-        }
-      } else {
-        setPostReturn(`No 'success' exists ${JSON.stringify(response)}`);
-      }
-    } catch {
-      // setLoading(false);
-      setPostReturn(`Failed to communicate with server`);
-    }
-  };
-
-  const handleClickStartPriceFetch = async () => {
-    const data = {
-      fetch_type: "CPU-Gaming",
-      product_list:
-        "AMD Ryzen 9 7900X,Intel Core i7-13700K,AMD Ryzen 7 5800X3D",
-    };
-    // setLoading(true);
-    try {
-      const response = await startPriceFetch(data);
-
-      // setLoading(false);
-
-      if (response.hasOwnProperty("success")) {
-        if (response.success) {
-          router.push(`/scrapes/${response.message}`);
-        } else {
-          setPostReturn(
-            `'Success' exists and is false ${JSON.stringify(response)}`
-          );
-        }
-      } else {
-        setPostReturn(`No 'success' exists ${JSON.stringify(response)}`);
-      }
-    } catch {
-      // setLoading(false);
-      setPostReturn(`Failed to communicate with server`);
-    }
-  };
-
-  const [postReturn, setPostReturn] = useState<string>("");
 
   const [loadingScrape, setLoadingScrape] = useState<boolean>(false);
 
@@ -193,19 +115,6 @@ export default function NewScrape() {
             </div>
           </div>
         </div>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <button onClick={handleClickTest} className="secondary">
-          TEST POST
-        </button>
-        <button onClick={handleClickStartPriceFetch} className="secondary">
-          START PRICE FETCH
-        </button>
       </CreateScrapeContext.Provider>
     </>
   );
