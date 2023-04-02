1813b844e3194fa296b67aa56bc078cf4c448bc8
Viktor <vikt.mag@gmail.com>
2023-04-02 20:54:13 +0200

    Added titles for Information page

--- app/frontend/app/information/page.tsx
+++ app/frontend/app/information/page.tsx
@@ -2,6 +2,10 @@ export default function Information() {
   return (
     <>
       <h1>Information</h1>
+      <h2>Tutorial</h2>
+      <h2>Benchmarks</h2>
+      <h3>Benchmark Disclaimer</h3>
+      <h2>Manual Comparison Tool</h2>
     </>
   );
 }
