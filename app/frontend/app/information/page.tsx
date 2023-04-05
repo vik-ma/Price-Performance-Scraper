0579eb2a2ec477b4ebdbf9de5b2f42659384b5b4
Viktor <vikt.mag@gmail.com>
2023-04-05 05:57:21 +0200

    Added Scrapes heading for Information page

--- app/frontend/app/information/page.tsx
+++ app/frontend/app/information/page.tsx
@@ -4,6 +4,8 @@ export default function Information() {
       <h1>Information</h1>
       <h2 className="information-tutorial">Tutorial</h2>
       <p></p>
+      <h2 className="information-scrapes">Scrapes</h2>
+      <p></p>
       <h2 className="information-benchmarks" id="benchmarks">Benchmarks</h2>
       <p></p>
       <h3 className="information-benchmarks-disclaimer">Benchmark Disclaimer</h3>
