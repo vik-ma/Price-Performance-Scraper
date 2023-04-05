0e3e5dade3a94c729605c02f4e966ff0119601da
Viktor <vikt.mag@gmail.com>
2023-04-05 05:53:00 +0200

    Added color gradients for the different Information headings

--- app/frontend/app/information/layout.tsx
+++ app/frontend/app/information/layout.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Additional Information",
+  title: "Information",
 };
 
 export default function RootLayout({
--- app/frontend/app/information/page.tsx
+++ app/frontend/app/information/page.tsx
@@ -2,13 +2,13 @@ export default function Information() {
   return (
     <>
       <h1>Information</h1>
-      <h2>Tutorial</h2>
+      <h2 className="information-tutorial">Tutorial</h2>
       <p></p>
-      <h2 id="benchmarks">Benchmarks</h2>
+      <h2 className="information-benchmarks" id="benchmarks">Benchmarks</h2>
       <p></p>
-      <h3>Benchmark Disclaimer</h3>
+      <h3 className="information-benchmarks-disclaimer">Benchmark Disclaimer</h3>
       <p></p>
-      <h2>Manual Comparison Tool</h2>
+      <h2 className="information-mct">Manual Comparison Tool</h2>
       <p></p>
     </>
   );
