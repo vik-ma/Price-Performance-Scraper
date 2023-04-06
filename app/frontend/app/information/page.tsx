580a2209800943359c52c082ad839d2933e5b067
Viktor <vikt.mag@gmail.com>
2023-04-06 23:25:34 +0200

    Changed Information sections

--- app/frontend/app/information/page.tsx
+++ app/frontend/app/information/page.tsx
@@ -2,10 +2,11 @@ export default function Information() {
   return (
     <>
       <h1 className="page-title">Information</h1>
-      <h2 className="information-tutorial">Tutorial</h2>
+      <h2 className="information-intro">How It Works</h2>
       <p></p>
-      <h2 className="information-scrapes">Scrapes</h2>
+      <h2 className="information-scrapes">Scraped Prices</h2>
       <p></p>
+      <h3 className="information-scrapes">Price / Performance Score</h3>
       <div className="centered-container">
         <div className="formula-container">asd</div>
       </div>
@@ -13,9 +14,7 @@ export default function Information() {
         Benchmarks
       </h2>
       <p></p>
-      <h3 className="information-benchmarks-disclaimer">
-        Benchmark Disclaimer
-      </h3>
+      <h3 className="information-benchmarks">Benchmark Disclaimer</h3>
       <p></p>
       <h2 className="information-mct">Manual Comparison Tool</h2>
       <p></p>
