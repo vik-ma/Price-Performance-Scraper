27e34cc4d8b14bc15d41b281cf12d8190e783cb9
Viktor <vikt.mag@gmail.com>
2023-04-18 12:40:59 +0200

    Added button to Start Scrape page in Home page

--- app/frontend/app/page.tsx
+++ app/frontend/app/page.tsx
@@ -8,6 +8,13 @@ export default async function Home() {
       <p className="home-text">
         <strong></strong>
       </p>
+      <div className="centered-container">
+        <Link href="/start_scrape">
+          <button className="start-button start-button-home">
+            <strong>Click Here To Start A Price Scrape</strong>
+          </button>
+        </Link>
+      </div>
     </main>
   );
 }
