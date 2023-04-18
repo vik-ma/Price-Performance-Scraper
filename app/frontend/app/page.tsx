91ca3fec5caafd83cdb82e8448cb3bdbd17dee32
Viktor <vikt.mag@gmail.com>
2023-04-18 13:02:28 +0200

    Added buttons to every page in Home page

--- app/frontend/app/page.tsx
+++ app/frontend/app/page.tsx
@@ -15,6 +15,34 @@ export default async function Home() {
           </button>
         </Link>
       </div>
+      <div className="centered-container">
+        <Link href="/manual_comparison">
+          <button className="start-button start-button-home mct-button-home">
+            <strong>Manual Comparison Tool</strong>
+          </button>
+        </Link>
+      </div>
+      <div className="centered-container">
+        <Link href="/benchmarks">
+          <button className="start-button start-button-home benchmarks-button-home">
+            <strong>See Current Benchmarks</strong>
+          </button>
+        </Link>
+      </div>
+      <div className="centered-container">
+        <Link href="/scrapes">
+          <button className="start-button start-button-home scrapes-button-home">
+            <strong>Browse Past Scrapes</strong>
+          </button>
+        </Link>
+      </div>
+      <div className="centered-container">
+        <Link href="/information">
+          <button className="start-button start-button-home info-button-home">
+            <strong>Learn More</strong>
+          </button>
+        </Link>
+      </div>
     </main>
   );
 }
