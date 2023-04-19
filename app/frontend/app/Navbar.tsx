61494245df6a42bc51ec03e83378a0f71cbf6c59
Viktor <vikt.mag@gmail.com>
2023-04-19 19:15:49 +0200

    Added HomeIcon SVG and replaced Home text in Navbar with HomeIcon

--- app/frontend/app/Navbar.tsx
+++ app/frontend/app/Navbar.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Link from "next/link";
+import HomeIcon from "./icons/HomeIcon";
 
 export default function Navbar() {
   return (
     <nav className="navbar-container">
       <div className="nav-items-wrapper container">
-        <Link className="nav-item" href="/">Home</Link>
+        <Link className="nav-item" href="/"><HomeIcon /></Link>
         <Link className="nav-item" href="/start_scrape">Start Price Scrape</Link>
         <Link className="nav-item" href="/information">Information</Link>
         <Link className="nav-item" href="/benchmarks">Current Benchmarks</Link>
---
+++ app/frontend/app/icons/HomeIcon.tsx
@@ -0,0 +1,15 @@
+export default function HomeIcon() {
+  return (
+    <svg
+      fill="#f2f2f2"
+      width="30px"
+      height="30px"
+      viewBox="0 0 32.00 32.00"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="#f2f2f2"
+      stroke-width="0.00032"
+    >
+      <path d="M27 18.039L16 9.501 5 18.039V14.56l11-8.54 11 8.538v3.481zm-2.75-.31v8.251h-5.5v-5.5h-5.5v5.5h-5.5v-8.25L16 11.543l8.25 6.186z"></path>
+    </svg>
+  );
+}
