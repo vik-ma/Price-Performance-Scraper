f65ef53c7546ed469b129388ee85bce23ff8015f
Viktor <vikt.mag@gmail.com>
2023-04-19 20:13:06 +0200

    Added MenuIcon to Navbar

--- app/frontend/app/Navbar.tsx
+++ app/frontend/app/Navbar.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import Link from "next/link";
 import HomeIcon from "./icons/HomeIcon";
+import MenuIcon from "./icons/MenuIcon";
 
 export default function Navbar() {
   return (
     <nav className="navbar-container">
       <div className="nav-items-wrapper container">
         <Link className="nav-item" id="home-icon" href="/"><HomeIcon /></Link>
-        <Link className="nav-item" href="/start_scrape">Start Price Scrape</Link>
+        <Link className="nav-item nav-item-extended" href="/start_scrape">Start Price Scrape</Link>
         <Link className="nav-item nav-item-extended" href="/information">Information</Link>
         <Link className="nav-item nav-item-extended" href="/benchmarks">Current Benchmarks</Link>
         <Link className="nav-item nav-item-extended" href="/manual_comparison">Manual Comparison Tool</Link>
         <Link className="nav-item nav-item-extended" href="/scrapes">Completed Price Scrapes</Link>
+        <div className="nav-item nav-item-menu"><MenuIcon /></div>
       </div>
     </nav>
   );
