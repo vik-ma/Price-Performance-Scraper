bd5ce9acd273d18d1af360c0da7d9f06b6a3affd
Viktor <vikt.mag@gmail.com>
2023-04-19 19:56:33 +0200

    Added MenuIcon

--- app/frontend/app/icons/HomeIcon.tsx
+++ app/frontend/app/icons/HomeIcon.tsx
@@ -7,7 +7,7 @@ export default function HomeIcon() {
       viewBox="0 4 32.00 32.00"
       xmlns="http://www.w3.org/2000/svg"
       stroke="#f2f2f2"
-      stroke-width="0.00032"
+      strokeWidth="0.00032"
     >
       <path d="M27 18.039L16 9.501 5 18.039V14.56l11-8.54 11 8.538v3.481zm-2.75-.31v8.251h-5.5v-5.5h-5.5v5.5h-5.5v-8.25L16 11.543l8.25 6.186z"></path>
     </svg>
---
+++ app/frontend/app/icons/MenuIcon.tsx
@@ -0,0 +1,19 @@
+export default function MenuIcon() {
+  return (
+    <svg
+      width="64px"
+      height="64px"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      stroke="#f2f2f2"
+      strokeWidth="0.8"
+    >
+      <path
+        fill="#f2f2f2"
+        fill-rule="evenodd"
+        d="M19 4a1 1 0 01-1 1H2a1 1 0 010-2h16a1 1 0 011 1zm0 6a1 1 0 01-1 1H2a1 1 0 110-2h16a1 1 0 011 1zm-1 7a1 1 0 100-2H2a1 1 0 100 2h16z"
+      ></path>
+    </svg>
+  );
+}
