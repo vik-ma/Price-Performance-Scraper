f6bde43f89cddb06b7c0780e58c821308861d4c4
Viktor <vikt.mag@gmail.com>
2023-04-19 17:34:38 +0200

    Added wrapper for Navbar link items

--- app/frontend/app/Navbar.tsx
+++ app/frontend/app/Navbar.tsx
@@ -4,12 +4,14 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <nav className="navbar-container">
-      <Link href="/">Home</Link>
-      <Link href="/start_scrape">Start Price Scrape</Link>
-      <Link href="/information">Information</Link>
-      <Link href="/benchmarks">Current Benchmarks</Link>
-      <Link href="/manual_comparison">Manual Comparison Tool</Link>
-      <Link href="/scrapes">Completed Price Scrapes</Link>
+      <div className="nav-items-wrapper container">
+        <Link href="/">Home</Link>
+        <Link href="/start_scrape">Start Price Scrape</Link>
+        <Link href="/information">Information</Link>
+        <Link href="/benchmarks">Current Benchmarks</Link>
+        <Link href="/manual_comparison">Manual Comparison Tool</Link>
+        <Link href="/scrapes">Completed Price Scrapes</Link>
+      </div>
     </nav>
   );
 }
