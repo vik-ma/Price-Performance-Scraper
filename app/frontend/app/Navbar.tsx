da11b425fd90a04309cd058408102996bfdb48ea
Viktor <vikt.mag@gmail.com>
2023-04-19 20:04:50 +0200

    Made Navbar items responsive

--- app/frontend/app/Navbar.tsx
+++ app/frontend/app/Navbar.tsx
@@ -8,10 +8,10 @@ export default function Navbar() {
       <div className="nav-items-wrapper container">
         <Link className="nav-item" id="home-icon" href="/"><HomeIcon /></Link>
         <Link className="nav-item" href="/start_scrape">Start Price Scrape</Link>
-        <Link className="nav-item" href="/information">Information</Link>
-        <Link className="nav-item" href="/benchmarks">Current Benchmarks</Link>
-        <Link className="nav-item" href="/manual_comparison">Manual Comparison Tool</Link>
-        <Link className="nav-item" href="/scrapes">Completed Price Scrapes</Link>
+        <Link className="nav-item nav-item-extended" href="/information">Information</Link>
+        <Link className="nav-item nav-item-extended" href="/benchmarks">Current Benchmarks</Link>
+        <Link className="nav-item nav-item-extended" href="/manual_comparison">Manual Comparison Tool</Link>
+        <Link className="nav-item nav-item-extended" href="/scrapes">Completed Price Scrapes</Link>
       </div>
     </nav>
   );
