e4c7fdc35e7ff7b277f55a5fd9d5d915c272df2f
Viktor <vikt.mag@gmail.com>
2023-04-20 03:54:19 +0200

    Added links to other pages to NavMenu list

--- app/frontend/app/NavMenu.tsx
+++ app/frontend/app/NavMenu.tsx
@@ -1,10 +1,37 @@
 "use client";
 import MenuIcon from "./icons/MenuIcon";
+import { useState } from "react";
+import Link from "next/link";
 
 export default function NavMenu() {
+  const [showFullMenu, setShowFullMenu] = useState<boolean>(false);
+
   return (
-    <button className="nav-item-menu">
-      <MenuIcon />
-    </button>
+    <>
+      <button
+        className="nav-item-menu"
+        onClick={() => setShowFullMenu(!showFullMenu)}
+      >
+        <MenuIcon />
+      </button>
+      {showFullMenu && (
+        <div className="nav-full-menu-wrapper">
+          <ul className="nav-full-menu">
+            <li className="nav-full-menu-item">
+              <Link href="/information">Information</Link>
+            </li>
+            <li className="nav-full-menu-item">
+              <Link href="/benchmarks">Current Benchmarks</Link>
+            </li>
+            <li className="nav-full-menu-item">
+              <Link href="/manual_comparison">Manual Comparison Tool</Link>
+            </li>
+            <li className="nav-full-menu-item">
+              <Link href="/scrapes">Completed Price Scrapes</Link>
+            </li>
+          </ul>
+        </div>
+      )}
+    </>
   );
 }
