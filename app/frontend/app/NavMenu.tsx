e67655a3cf042807210d27f7f031c3ace9483a28
Viktor <vikt.mag@gmail.com>
2023-04-20 03:30:54 +0200

    Changed nav-item-menu from div to button

--- app/frontend/app/NavMenu.tsx
+++ app/frontend/app/NavMenu.tsx
@@ -3,8 +3,8 @@ import MenuIcon from "./icons/MenuIcon";
 
 export default function NavMenu() {
   return (
-    <div className="nav-item-menu">
+    <button className="nav-item-menu">
       <MenuIcon />
-    </div>
+    </button>
   );
 }
