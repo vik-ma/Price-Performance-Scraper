dca37485bddb0ab73f268d603a3c47172eea02cb
Viktor <vikt.mag@gmail.com>
2023-04-22 07:11:54 +0200

    Fixed MenuIcon property

--- app/frontend/app/icons/MenuIcon.tsx
+++ app/frontend/app/icons/MenuIcon.tsx
@@ -11,7 +11,7 @@ export default function MenuIcon() {
     >
       <path
         fill="#f2f2f2"
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M19 4a1 1 0 01-1 1H2a1 1 0 010-2h16a1 1 0 011 1zm0 6a1 1 0 01-1 1H2a1 1 0 110-2h16a1 1 0 011 1zm-1 7a1 1 0 100-2H2a1 1 0 100 2h16z"
       ></path>
     </svg>
