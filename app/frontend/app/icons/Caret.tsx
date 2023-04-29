08f3113932321f33f8c5642648f160db3c85018c
Viktor <vikt.mag@gmail.com>
2023-04-29 23:45:34 +0200

    Changed color of Caret icon and added media queries to slightly adjust positioning of arrow-right-side on smaller screens

--- app/frontend/app/icons/Caret.tsx
+++ app/frontend/app/icons/Caret.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 
 export default function Caret({ rotate = 0 }) {
-    return (
-      <svg viewBox="0 0 24 24" width="36" height="36" style={{ transform: `rotate(${rotate}deg)` }}>
-        <path fill="#00d9ff" d="M7 10l5 5 5-5z" />
-      </svg>
-    );
-  }
+  return (
+    <svg
+      fill="#00df6f"
+      width="36"
+      height="36"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      style={{ transform: `rotate(${rotate}deg)` }}
+    >
+      <path d="M7 10l5 5 5-5z" />
+    </svg>
+  );
+}
