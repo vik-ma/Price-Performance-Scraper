854129cb04319ff682cd612f02ca8632be036d1d
Viktor <vikt.mag@gmail.com>
2023-05-12 19:23:07 +0200

    Made CORS_ALLOWED_ORIGINS to env variable

--- app/backend/backend/settings.py
+++ app/backend/backend/settings.py
@@ -137,7 +137,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:3000',
-    'http://localhost:3000'
-]
+CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS')
