a130f0e8229660b114c527c096c0001e5ed74f8c
Viktor <vikt.mag@gmail.com>
2023-05-13 21:18:16 +0200

    Modified benchmark_updater

--- app/backend/jobs/benchmark_updater.py
+++ app/backend/jobs/benchmark_updater.py
@@ -8,7 +8,7 @@ def start():
         every day at 07:00 Swedish time.
     """
     # Stop job from running twice
-    if os.environ.get('RUN_MAIN'):
-        scheduler = BackgroundScheduler(timezone="Europe/Stockholm")
-        scheduler.add_job(pf.update_benchmarks, 'cron', hour=21, minute=14)
-        scheduler.start()
+    # if os.environ.get('RUN_MAIN'):
+    scheduler = BackgroundScheduler(timezone="Europe/Stockholm")
+    scheduler.add_job(pf.update_benchmarks, 'cron', hour=21, minute=23)
+    scheduler.start()
