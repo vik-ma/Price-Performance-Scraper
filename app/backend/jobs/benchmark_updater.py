294bb52b8a7ab39001efa687c6d5d54d47de8ffa
Viktor <vikt.mag@gmail.com>
2023-05-13 21:33:41 +0200

    Changed benchmark_updater to run once in dev and also on prod

--- app/backend/jobs/benchmark_updater.py
+++ app/backend/jobs/benchmark_updater.py
@@ -5,10 +5,14 @@ import price_fetcher.views as pf
 def start():
     """
         Start a job that will attempt to update and replace existing benchmarks 
-        every day at 07:00 Swedish time.
+        every day at 22:00 Swedish time.
     """
-    # Stop job from running twice
-    # if os.environ.get('RUN_MAIN'):
-    scheduler = BackgroundScheduler(timezone="Europe/Stockholm")
-    scheduler.add_job(pf.update_benchmarks, 'cron', hour=21, minute=23)
-    scheduler.start()
+    # Stop job from running twice in dev
+    if os.environ.get('RUN_MAIN'):
+        scheduler = BackgroundScheduler(timezone="Europe/Stockholm")
+        scheduler.add_job(pf.update_benchmarks, 'cron', hour=22, minute=00)
+        scheduler.start()
+    else:
+        scheduler = BackgroundScheduler(timezone="Europe/Stockholm")
+        scheduler.add_job(pf.update_benchmarks, 'cron', hour=22, minute=00)
+        scheduler.start()
