f60c3ae9b72af5e2e4744a01d5b8718172805fce
Viktor <vikt.mag@gmail.com>
2023-07-09 13:20:32 +0200

    Added GeForce RTX 4060 to benchmark_scraper

--- app/backend/price_fetcher/benchmark_scraper.py
+++ app/backend/price_fetcher/benchmark_scraper.py
@@ -35,6 +35,7 @@ list_of_gpus_to_scrape = [
     "Radeon RX 6750 XT",
     "GeForce RTX 3060 Ti",
     "Radeon RX 6700 XT",
+    "GeForce RTX 4060",
     "Radeon RX 6700",
     "Radeon RX 7600",
     "Radeon RX 6650 XT",
