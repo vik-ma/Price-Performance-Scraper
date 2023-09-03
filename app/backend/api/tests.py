d90334c0c4da8014ed15a8d4d9a768ade14a34c2
Viktor <vikt.mag@gmail.com>
2023-09-03 23:30:20 +0200

    Added test cases for invalid fetch_types

--- app/backend/api/tests.py
+++ app/backend/api/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from api.views import valid_cpu_set, valid_gpu_set, validate_price_fetch_request
 from api.serializers import FetchPropertiesSerializer
-
+from rest_framework import serializers
 
 # Create your tests here.
 class TestValidPriceFetchRequest(TestCase):
@@ -21,7 +21,6 @@ class TestValidPriceFetchRequest(TestCase):
                 "product_list": cpu
             }
             serializer = FetchPropertiesSerializer(data=request)
-            
             self.assertEqual(True, serializer.is_valid())
     
 
@@ -32,7 +31,6 @@ class TestValidPriceFetchRequest(TestCase):
                 "product_list": cpu
             }
             serializer = FetchPropertiesSerializer(data=request)
-            
             self.assertEqual(True, serializer.is_valid())
 
 
@@ -43,6 +41,55 @@ class TestValidPriceFetchRequest(TestCase):
                 "product_list": gpu
             }
             serializer = FetchPropertiesSerializer(data=request)
-            
             self.assertEqual(True, serializer.is_valid())
 
+
+    def test_valid_requests(self):
+        request_cpu_g = { 
+            "fetch_type": self.cpu_g_fetch_type, 
+            "product_list": "AMD Ryzen 9 7950X3D"
+        }
+        request_cpu_n = { 
+            "fetch_type": self.cpu_n_fetch_type, 
+            "product_list": "AMD Ryzen 9 7950X3D"
+        }
+        request_gpu = { 
+            "fetch_type": self.gpu_fetch_type, 
+            "product_list": "GeForce RTX 4090"
+        }
+        requests = [request_cpu_g, request_cpu_n, request_gpu]
+        for request in requests:
+            serializer = FetchPropertiesSerializer(data=request)
+            self.assertEqual(True, serializer.is_valid())
+
+    
+    def test_invalid_fetch_types(self):
+        request_wrong_name = {
+            "fetch_type": "", 
+            "product_list": "AMD Ryzen 9 7950X3D"
+        }
+        request_wrong_type = {
+            "fetch_type": ["CPU-Gaming"], 
+            "product_list": "AMD Ryzen 9 7950X3D"
+        }
+        requests = [request_wrong_name, request_wrong_type]
+        for request in requests:
+            serializer = FetchPropertiesSerializer(data=request)
+            self.assertNotEqual(True, serializer.is_valid())
+
+
+    def test_mismatched_products_and_fetch_type(self):
+        request_gpu_fetch_cpu_product = {
+            "fetch_type": self.gpu_fetch_type, 
+            "product_list": "AMD Ryzen 9 7950X3D"
+        }
+        request_cpu_fetch_gpu_product = {
+            "fetch_type": self.cpu_n_fetch_type, 
+            "product_list": "GeForce RTX 4090"
+        }
+        requests = [request_cpu_fetch_gpu_product, request_gpu_fetch_cpu_product]
+        for request in requests:
+            serializer = FetchPropertiesSerializer(data=request)
+            with self.assertRaises(serializers.ValidationError):
+                if serializer.is_valid():
+                    validate_price_fetch_request(serializer.data)
