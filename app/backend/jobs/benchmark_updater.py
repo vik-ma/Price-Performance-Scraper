e071159c0ba992821f2233a9b327762db7c5ad3d
Viktor <vikt.mag@gmail.com>
2023-10-13 16:45:01 +0200

    Removed limiter from benchmark_updater

--- app/backend/jobs/benchmark_updater.py
+++ app/backend/jobs/benchmark_updater.py
@@ -1,24 +1,11 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 import price_fetcher.views as pf
 
-class BenchmarkScrapeSessionLimiter():
-    def __init__(self):
-        self.is_scrape_running = False
-
-scrape_limiter = BenchmarkScrapeSessionLimiter()
-
 def start():
     """
         Start a job that will attempt to update and replace existing benchmarks 
         every day at 22:00 Swedish time.
     """
     scheduler = BackgroundScheduler(timezone="Europe/Stockholm")
-    scheduler.add_job(update_benchmarks, 'cron', hour=22, minute=00, misfire_grace_time=None)
+    scheduler.add_job(pf.update_benchmarks, 'cron', hour=22, minute=00, misfire_grace_time=None)
     scheduler.start()
-
-
-def update_benchmarks():
-    if not scrape_limiter.is_scrape_running:
-        scrape_limiter.is_scrape_running = True
-        pf.update_benchmarks()
-        scrape_limiter.is_scrape_running = False
