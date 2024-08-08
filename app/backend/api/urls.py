f450589e8ea0b89c4a5f56f4839685e8e96167c5
Viktor <vikt.mag@gmail.com>
2024-08-08 14:05:47 +0200

    Disable correct URLs

--- app/backend/api/urls.py
+++ app/backend/api/urls.py
@@ -8,10 +8,10 @@ urlpatterns = [
     # path('test_get/', views.test_frontend),   # Test GET request for debugging purposes
     # path('test_post/', views.test_post),      # Test POST request for debugging purposes
     # path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=False, schema=schema))), # GraphQL is not currently used
-    # path('start_price_fetch/', views.start_price_fetch),
-    # path('get_benchmarks/', views.get_benchmarks),
-    # path('get_scrape_allowed/', views.get_scrape_allowed),
-    # path('get_all_completed_fetches/', views.get_all_completed_fetches),
-    # path('get_completed_fetch_by_timestamp_id/<timestamp_id>/', views.get_completed_fetch_by_timestamp_id),
-    # path('get_product_listings_from_timestamp_id/<timestamp_id>/', views.get_product_listings_from_timestamp_id),
+    path('start_price_fetch/', views.start_price_fetch),
+    path('get_benchmarks/', views.get_benchmarks),
+    path('get_scrape_allowed/', views.get_scrape_allowed),
+    path('get_all_completed_fetches/', views.get_all_completed_fetches),
+    path('get_completed_fetch_by_timestamp_id/<timestamp_id>/', views.get_completed_fetch_by_timestamp_id),
+    path('get_product_listings_from_timestamp_id/<timestamp_id>/', views.get_product_listings_from_timestamp_id),
 ]
